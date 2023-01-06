50172b8745202da72dc13c18252b70e617efffec
natnael-beru <120000938+natnael-beru@users.noreply.github.com>
2023-01-06 09:36:57 -0800

    Update serializers.py

--- student/serializers.py
+++ student/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import studentclass
-class stuserializer(serializers.ModelSerializer):
+class studentserializer(serializers.ModelSerializer):
                 class Meta:
                     model=stuclass
                     fields='__all__'
