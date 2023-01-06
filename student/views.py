d137d7cf46838780ef9a0b26a4df1c776264e599
natnael-beru <120000938+natnael-beru@users.noreply.github.com>
2023-01-06 09:38:56 -0800

    Update views.py

--- student/views.py
+++ student/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import studentclass
 from rest_framework import generics
-from .serializers import stuserializer
+from .serializers import studentserializer
 class studentlist(generics.ListCreateAPIView):
                    queryset=studentclass.objects.all()
-                   serializer_class=stuserializer
+                   serializer_class=studentserializer
 class studentdel(generics.RetrieveUpdateDestroyAPIView):
                    queryset=studentclass.objects.all()
-                   serializer_class=stuserializer
+                   serializer_class=studentserializer
